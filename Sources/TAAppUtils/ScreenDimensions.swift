be301ff34104124a4121fcf529ec24bf84ff8d11
Tataru Robert <robert.tataru98@gmail.com>
2025-06-04 15:12:25 +0300

    Changed to public access

--- Sources/TAAppUtils/ScreenDimensions.swift
+++ Sources/TAAppUtils/ScreenDimensions.swift
@@ -11,59 +11,59 @@ import UIKit
 /// Utility for accessing screen and device-specific properties.
 public enum ScreenDimensions {
     // MARK: - Screen Properties
-    static let screenWidth: CGFloat = UIScreen.main.bounds.width
-    static let screenHeight: CGFloat = UIScreen.main.bounds.height
+    static public let screenWidth: CGFloat = UIScreen.main.bounds.width
+    static public let screenHeight: CGFloat = UIScreen.main.bounds.height
     
-    // MARK: - Device Heights
-    static let iPhone8Height: CGFloat = 667.0
-    static let iPhoneXHeight: CGFloat = 812.0
-    static let iPhone11ProHeight: CGFloat = 896.0
-    static let iPhone12ProHeight: CGFloat = 844.0
-    static let iPhone14ProMaxHeight: CGFloat = 932.0
+    // MAR publicK: - Device Heights
+    static public let iPhone8Height: CGFloat = 667.0
+    static public let iPhoneXHeight: CGFloat = 812.0
+    static public let iPhone11ProHeight: CGFloat = 896.0
+    static public let iPhone12ProHeight: CGFloat = 844.0
+    static public let iPhone14ProMaxHeight: CGFloat = 932.0
 
-    // MARK: - Device Widths (for completeness)
-    static let iPhone8Width: CGFloat = 375.0
-    static let iPhoneXWidth: CGFloat = 375.0
-    static let iPhone11ProWidth: CGFloat = 414.0
-    static let iPhone12ProWidth: CGFloat = 390.0
-    static let iPhone14ProMaxWidth: CGFloat = 430.0
+    // MAR publicK: - Device Widths (for completeness)
+    static public let iPhone8Width: CGFloat = 375.0
+    static public let iPhoneXWidth: CGFloat = 375.0
+    static public let iPhone11ProWidth: CGFloat = 414.0
+    static public let iPhone12ProWidth: CGFloat = 390.0
+    static public let iPhone14ProMaxWidth: CGFloat = 430.0
     
     // MARK: - Screen Comparison Methods
-    static func isBiggerThan(height: CGFloat) -> Bool {
+    static public func isBiggerThan(height: CGFloat) -> Bool {
         screenHeight > height
     }
     
-    static func isSmallerThan(height: CGFloat) -> Bool {
+    static public func isSmallerThan(height: CGFloat) -> Bool {
         screenHeight < height
     }
     
-    static func isEqualTo(height: CGFloat) -> Bool {
+    static public func isEqualTo(height: CGFloat) -> Bool {
         screenHeight == height
     }
     
     // MARK: - iPhone Specific Checks
-    static var isIphone8: Bool {
+    static public var isIphone8: Bool {
         return isEqualTo(height: iPhone8Height)
     }
     
-    static var isIphoneX: Bool {
+    static public  var isIphoneX: Bool {
         return isEqualTo(height: iPhoneXHeight)
     }
     
-    static var isIphone11Pro: Bool {
+    static public  var isIphone11Pro: Bool {
         return isEqualTo(height: iPhone11ProHeight)
     }
     
-    static var isIphone12Pro: Bool {
+    static public var isIphone12Pro: Bool {
         return isEqualTo(height: iPhone12ProHeight)
     }
     
-    static var isIphone14ProMax: Bool {
+    static public var isIphone14ProMax: Bool {
         return isEqualTo(height: iPhone14ProMaxHeight)
     }
     
     // MARK: - Safe Area Check
-    static var hasBottomSafeArea: Bool {
+    static public var hasBottomSafeArea: Bool {
         UIApplication.shared.connectedScenes
             .compactMap { $0 as? UIWindowScene }
             .first?
@@ -73,29 +73,29 @@ public enum ScreenDimensions {
     }
     
     // MARK: - Orientation Checks
-    static var isPortrait: Bool {
+    static public var isPortrait: Bool {
         return screenHeight > screenWidth
     }
     
-    static var isLandscape: Bool {
+    static public var isLandscape: Bool {
         return screenWidth > screenHeight
     }
     
     // MARK: - Scale Factor
     static let screenScale: CGFloat = UIScreen.main.scale
     
-    static var isRetina: Bool {
+    static public var isRetina: Bool {
         return screenScale >= 2.0
     }
     
     // MARK: - Dynamic Type Support
-    static let preferredContentSizeCategory = UIApplication.shared.preferredContentSizeCategory
+    static public let preferredContentSizeCategory = UIApplication.shared.preferredContentSizeCategory
     
-    static func isAccessibilityCategory() -> Bool {
+    static public func isAccessibilityCategory() -> Bool {
         return preferredContentSizeCategory.isAccessibilityCategory
     }
     
-    static func isLargerThanLargeCategory() -> Bool {
+    static public func isLargerThanLargeCategory() -> Bool {
         return preferredContentSizeCategory > .large
     }
 }
